757a46b3b0e8b774da1be952db67f9147d3c2aab
pranwagh11 <pranwagh11@gmail.com>
2026-06-12 12:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const display = document.getElementById("display");
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+
+function getExpression() {
+    return display.value;
+}
+
+function setDisplay(value) {
+    display.value = value;
+}
+
+function evaluateExpression(expression) {
+    return eval(expression);
+}
+
+function calculate() {
+    try {
+        const expression = getExpression();
+        const result = evaluateExpression(expression);
+
+        setDisplay(result);
+    } catch {
+        setDisplay("Error");
+    }
+}
 
